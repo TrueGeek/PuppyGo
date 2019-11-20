f827fca9efb45822b63fa2bdc47d5e991217e751
Brad Dean <bdean@truegeek.com>
2019-11-20 10:11:06 -0500

    trying post build at ~/.nuget/packages/xamarin.uitest

--- PuppyGo.iOS/appcenter-post-build.sh
+++ PuppyGo.iOS/appcenter-post-build.sh
@@ -12,10 +12,10 @@ echo "**************************************************************************
 # Start UI Tests
 ##################################################
 
-echo "> Run UI test command: --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools "
+echo "> Run UI test command: ~/.nuget/packages/xamarin.uitest "
 # appCenterLoginApiToken must be setup as an environment variable for this branch. see this link for how to get the token
 # https://docs.microsoft.com/en-us/appcenter/api-docs/
-appcenter test run uitest --app "LivePersonBrad/PuppyGo-1" --devices e7a1a0a7 --app-path $APPCENTER_OUTPUT_DIRECTORY/PuppyGo.ipa --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/PuppyGo.UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken --debug
+appcenter test run uitest --app "LivePersonBrad/PuppyGo-1" --devices e7a1a0a7 --app-path $APPCENTER_OUTPUT_DIRECTORY/PuppyGo.ipa --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/PuppyGo.UITests/bin/Debug --uitest-tools-dir ~/.nuget/packages/xamarin.uitest --token $appCenterLoginApiToken --debug
 
 echo ""
 echo "**************************************************************************************************"
