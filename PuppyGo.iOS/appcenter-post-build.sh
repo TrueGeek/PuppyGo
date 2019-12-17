081c85960265188eb34563c130277e85f7d78b92
Brad Dean <bdean@liveperson.com>
2019-12-17 09:47:51 -0500

    moving ui-tools path to root level

--- PuppyGo.iOS/appcenter-post-build.sh
+++ PuppyGo.iOS/appcenter-post-build.sh
@@ -15,20 +15,14 @@ echo "**************************************************************************
 echo "> Build UI test project"
 msbuild "$APPCENTER_SOURCE_DIRECTORY/PuppyGo.UITests/PuppyGo.UITests.csproj" /property:Configuration=debug
 
-echo "> Contents of $APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS"
-for entry in "$APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS"/*
+echo "> Contents of $APPCENTER_SOURCE_DIRECTORY/packages"
+for entry in "$APPCENTER_SOURCE_DIRECTORY/packages"/*
 do
     echo "$entry"
 done
 
-echo "> Contents of $APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS/packages"
-for entry in "$APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS/packages"/*
-do
-    echo "$entry"
-done
-
-echo "> Contents of $APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS/packages/Xamarin.UITest.*/tools"
-for entry in "$APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS/packages/Xamarin.UITest.*/tools"/*
+echo "> Contents of $APPCENTER_SOURCE_DIRECTORY/src/packages"
+for entry in "$APPCENTER_SOURCE_DIRECTORY/src/packages"/*
 do
     echo "$entry"
 done
@@ -36,7 +30,7 @@ done
 echo "> Run UI test command"
 # appCenterLoginApiToken must be setup as an environment variable for this branch. see this link for how to get the token
 # https://docs.microsoft.com/en-us/appcenter/api-docs/
-appcenter test run uitest --app "LivePersonBrad/PuppyGo-1" --devices e7a1a0a7 --app-path $APPCENTER_OUTPUT_DIRECTORY/PuppyGo.ipa --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/PuppyGo.UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/PuppyGo.iOS/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken --debug
+appcenter test run uitest --app "LivePersonBrad/PuppyGo-1" --devices e7a1a0a7 --app-path $APPCENTER_OUTPUT_DIRECTORY/PuppyGo.ipa --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/PuppyGo.UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken --debug
 
 echo ""
 echo "**************************************************************************************************"
